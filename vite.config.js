c67db871049b29d7128bbccc3ca3bc34a0d58ce2
Aaron Blondeau <ablondeau@dose.com>
2022-03-04 11:30:35 -0700

    Tweak vite config to help cli run in both dev and release.

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL(path.resolve(__dirname, 'src'), import.meta.url))
     }
+  },
+  server: {
+    port: 3001
   }
 })
